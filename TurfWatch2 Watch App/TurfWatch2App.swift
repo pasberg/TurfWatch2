4066d2d46a7e3375000bcbf45c885ea18964580e
pasberg <pasberg@pasberg.com>
2023-03-27 15:23:29 +0200

    Initial Commit

---
+++ TurfWatch2 Watch App/TurfWatch2App.swift
@@ -0,0 +1,17 @@
+//
+//  TurfWatch2App.swift
+//  TurfWatch2 Watch App
+//
+//  Created by Peter Åsberg on 2023-03-27.
+//
+
+import SwiftUI
+
+@main
+struct TurfWatch2_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
